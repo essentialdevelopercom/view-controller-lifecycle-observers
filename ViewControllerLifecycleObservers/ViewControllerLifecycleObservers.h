849373c953e0e99819c7ee3eaad805a5d05d0c8a
Caio Zullo <caiozullo@gmail.com>
2018-07-30 12:14:39 +0100

    Initial Commit

---
+++ ViewControllerLifecycleObservers/ViewControllerLifecycleObservers.h
@@ -0,0 +1,19 @@
+//
+//  ViewControllerLifecycleObservers.h
+//  ViewControllerLifecycleObservers
+//
+//  Created by Caio Zullo on 30/07/2018.
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewControllerLifecycleObservers.
+FOUNDATION_EXPORT double ViewControllerLifecycleObserversVersionNumber;
+
+//! Project version string for ViewControllerLifecycleObservers.
+FOUNDATION_EXPORT const unsigned char ViewControllerLifecycleObserversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewControllerLifecycleObservers/PublicHeader.h>
+
+
